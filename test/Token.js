24977939f14d01e9265869a507a632cf0ea5493c
Maciej Kurantowicz <kurantowicz.maciej@gmail.com>
2022-07-26 22:47:57 +0200

    4. Token Balances

--- test/Token.js
+++ test/Token.js
@@ -6,10 +6,14 @@ const tokens =(n)=>{
 }
 
 describe('Token', ()=> {
-    let token 
+    let token , accounts, deployer
+
     beforeEach(async() => {
         const Token = await ethers.getContractFactory('Token')
         token = await Token.deploy('Dupa szkolenia', 'DUPA', '1000000')
+
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
     })
 
     describe('Deployment', () => {
@@ -29,6 +33,9 @@ describe('Token', ()=> {
         })
         it('has correct total supply', async ()=>{
             expect(await token.totalSupply()).to.equal(totalSupply)
+        })
+        it('assigns total supply to deployer', async ()=>{
+            expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
         })
     })
 
