18b7c325b8e966af2b016050bd1aa0d52df8bfea
Maciej Kurantowicz <kurantowicz.maciej@gmail.com>
2022-07-26 17:36:26 +0200

    2. Smoke tets

---
+++ contracts/Token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+// Import this file to use console.log
+import "hardhat/console.sol";
+
+contract Token{
+    string public name = "My Token";
+}
+
