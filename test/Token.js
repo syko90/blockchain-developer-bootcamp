5ab12f870e6e04bf2902eda28c6d30c024ee0918
Maciej Kurantowicz <kurantowicz.maciej@gmail.com>
2022-07-26 21:57:43 +0200

    3. Testing Contracts

---
+++ test/Token.js
@@ -0,0 +1,36 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens =(n)=>{
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe('Token', ()=> {
+    let token 
+    beforeEach(async() => {
+        const Token = await ethers.getContractFactory('Token')
+        token = await Token.deploy('Dupa szkolenia', 'DUPA', '1000000')
+    })
+
+    describe('Deployment', () => {
+        const name = 'Dupa szkolenia'
+        const symbol = 'DUPA'
+        const decimals = '18'
+        const totalSupply = tokens('1000000')
+
+        it('has a name', async ()=>{
+            expect(await token.name()).to.equal(name)
+        })
+        it('has correct symbol', async ()=>{
+            expect(await token.symbol()).to.equal(symbol)
+        })
+        it('has correct decimals', async ()=>{
+            expect(await token.decimals()).to.equal(decimals)
+        })
+        it('has correct total supply', async ()=>{
+            expect(await token.totalSupply()).to.equal(totalSupply)
+        })
+    })
+
+
+})
